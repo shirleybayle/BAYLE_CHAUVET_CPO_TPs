ffb51b7f7424b15a2612bd972791653961ce56f0
shirl <shirl@DESKTOP-FJJIUNF>
2021-10-07 22:18:43 +0200

    création du dépot

---
+++ Exo1.test/src/exo1/test/Exo1Test.java
@@ -0,0 +1,27 @@
+/*
+ * TP0 Exercice 1
+ * BAYLE Shirley TDC
+ * 27 Septembre 2021
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shirl
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prénom?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir");
+    }
+    
