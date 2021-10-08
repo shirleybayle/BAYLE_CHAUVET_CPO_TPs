9a39965863a99373b4c49545c8c99ba8bab128a4
shirl <shirl@DESKTOP-FJJIUNF>
2021-10-08 14:46:06 +0200

    jusqu'à question 3

--- TP1_guessMyNumber_CHAUVET_BAYLE/src/tp1_guessmynumber_chauvet_bayle/TP1_guessMyNumber_CHAUVET_BAYLE.java
+++ TP1_guessMyNumber_CHAUVET_BAYLE/src/tp1_guessmynumber_chauvet_bayle/TP1_guessMyNumber_CHAUVET_BAYLE.java
@@ -6,6 +6,7 @@
 package tp1_guessmynumber_chauvet_bayle;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -18,8 +19,25 @@ public class TP1_guessMyNumber_CHAUVET_BAYLE {
      */
     public static void main(String[] args) {
         Random generateurAleat = new Random();
-        int test = generateurAleat.nextInt();
-        System.out.println(test);
+        int n = generateurAleat.nextInt(100);
+        Scanner sc = new Scanner(System.in);
+        int nbtest = -1;
+        int compteur = 0;
+        while (nbtest!=n) {
+        compteur = compteur+1;
+        System.out.println("Saisissez un nombre entre 0 et 100");
+        nbtest = sc.nextInt();
+        if (nbtest<n) {
+            System.out.println("trop petit");
+        }
+        else if (nbtest>n) {
+            System.out.println("trop grand");
+        }
+        else {
+            System.out.println("gagné !");
+        }
+        }
+        System.out.println("Vous avez trouvé le nombre en "+compteur+" tentatives");
     }
     
 }
