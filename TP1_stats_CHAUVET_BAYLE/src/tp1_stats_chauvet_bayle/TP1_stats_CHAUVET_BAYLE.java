bb79a92ab868f9923703e1c951465e203c37af83
shirl <shirl@DESKTOP-FJJIUNF>
2021-10-08 18:54:30 +0200

    statistiques dé création

---
+++ TP1_stats_CHAUVET_BAYLE/src/tp1_stats_chauvet_bayle/TP1_stats_CHAUVET_BAYLE.java
@@ -0,0 +1,40 @@
+/*
+ * CHAUVET Ylan - BAYLE Shirley
+ * Exercice statistiques TP1 CPO
+ * 08/10/2021
+ */
+package tp1_stats_chauvet_bayle;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author shirl
+ */
+public class TP1_stats_CHAUVET_BAYLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] tab = new int[6];
+        Random generateurAleat = new Random();
+        for (int i=0; i<6; i++) {
+            tab[i] = 0;
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez un entier.");
+        int m = sc.nextInt();
+        for (int i=0; i<m; i++) {
+            int indice = generateurAleat.nextInt(6);
+            tab[indice] = tab[indice]+1;
+        }
+        for (int i=0; i<6; i++) {
+            int valeur = tab[i];
+            System.out.println("La face numéro "+(i+1)+" du dé a été tirée "+((valeur*100.0)/m)+"% de fois.");
+        }
+        
+    }
+    
+}
