ca87a659712b2d794bae74bd0a50ffcfa3ea698c
shirl <shirl@DESKTOP-FJJIUNF>
2021-10-08 18:09:00 +0200

    ajout des niveaux 2 et 3

--- TP1_guessMyNumber_CHAUVET_BAYLE/src/tp1_guessmynumber_chauvet_bayle/TP1_guessMyNumber_CHAUVET_BAYLE.java
+++ TP1_guessMyNumber_CHAUVET_BAYLE/src/tp1_guessmynumber_chauvet_bayle/TP1_guessMyNumber_CHAUVET_BAYLE.java
@@ -19,20 +19,42 @@ public class TP1_guessMyNumber_CHAUVET_BAYLE {
      */
     public static void main(String[] args) {
         Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
         Scanner sc = new Scanner(System.in);
         int nbtest = -1;
+        int n;
         System.out.println("Choisissez un niveau de diffciulté : \n1) Facile \n2) Intermédiaire \n3) Difficile");
         int niveau = sc.nextInt();
+        if (niveau != 3) {
+        n = generateurAleat.nextInt(100);
+        }
+        else {
+            n = generateurAleat.nextInt(1000);
+        }
         while (niveau<1 || niveau>3) {
             System.out.println("Erreur, veuillez saisir un nombre entre 1 et 3.");
             niveau = sc.nextInt();
         }
-       
+        if (niveau == 1) {
+            System.out.println("Vous avez choisi le niveau facile.");
+        }
+        else if (niveau == 2) {
+            System.out.println("Vous avez choisi le niveau intermédiaire, vous avez 8 tentatives pour trouver le nombre.");
+        }
+        else if (niveau == 3) {
+            System.out.println("Vous avez choisi le niveau difficile, vous devrez trouver un nombre entre 0 et 1000.");
+        }
         int compteur = 0;
         while (nbtest!=n) {
+        if (compteur == 8 && niveau == 2) {
+            break;
+        }
         compteur = compteur+1;
+        if (niveau != 3) {
         System.out.println("Saisissez un nombre entre 0 et 100");
+        }
+        else {
+            System.out.println("Saisissez un nombre entre 0 et 1000");
+        }
         nbtest = sc.nextInt();
         if (nbtest<n) {
             if (niveau == 1 && nbtest+20<n) {
@@ -55,7 +77,12 @@ public class TP1_guessMyNumber_CHAUVET_BAYLE {
             System.out.println("gagné !");
         }
         }
+        if (compteur == 8 && niveau == 2) {
+            System.out.println("Vous n'avez pas réussi à trouver le nombre en moins de 8 tentatives, vous avez perdu.");
+        }
+        else {
         System.out.println("Vous avez trouvé le nombre en "+compteur+" tentatives");
+        }
     }
     
 }
