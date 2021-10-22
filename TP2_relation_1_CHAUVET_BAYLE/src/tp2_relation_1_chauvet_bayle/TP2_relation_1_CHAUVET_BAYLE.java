b464e60edb0b70135dbb44374ac41508c51dc39f
shirl <shirl@DESKTOP-FJJIUNF>
2021-10-22 11:14:34 +0200

    création TP2 relation entre objets

---
+++ TP2_relation_1_CHAUVET_BAYLE/src/tp2_relation_1_chauvet_bayle/TP2_relation_1_CHAUVET_BAYLE.java
@@ -0,0 +1,21 @@
+/*
+ * CHAUVET Ylan - BAYLE Shirley
+ * TP2 Cours Programmation Objet 
+ * Exercice relation entre objets
+ */
+package tp2_relation_1_chauvet_bayle;
+
+/**
+ *
+ * @author shirl
+ */
+public class TP2_relation_1_CHAUVET_BAYLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
