dfbc07d4d1936d554ad079e0a18ec8d7768baf9f
ylanc <ylanc@192.168.114.123>
2021-10-22 11:36:52 +0200

    commentaires

--- TP2_manip_CHAUVET_BAYLE/src/tp2_manip_chauvet_bayle/Moussaka.java
+++ TP2_manip_CHAUVET_BAYLE/src/tp2_manip_chauvet_bayle/Moussaka.java
@@ -7,9 +7,9 @@ package tp2_manip_chauvet_bayle;
  *
  * @author shirl
  */
-public class Moussaka {
+public class Moussaka { //Création d'une classe public réutilisable dans d'autres .java
     public Moussaka(int kcal) {
         nbCalories = kcal;
     }
-    int nbCalories;
+    int nbCalories; //Demande et affecte le nombre de Calories de la Moussaka
 }
--- TP2_manip_CHAUVET_BAYLE/src/tp2_manip_chauvet_bayle/TP2_manip_CHAUVET_BAYLE.java
+++ TP2_manip_CHAUVET_BAYLE/src/tp2_manip_chauvet_bayle/TP2_manip_CHAUVET_BAYLE.java
@@ -11,9 +11,7 @@ package tp2_manip_chauvet_bayle;
  */
 public class TP2_manip_CHAUVET_BAYLE {
 
-    /**
-     * @param args the command line arguments
-     */
+    //On réutilise les classe Moussaka et Tartiflette dnas d'autres .java
     public static void main(String[] args) {
         Tartiflette assiette1 = new Tartiflette(500);
         Tartiflette assiette2 = new Tartiflette(600);
@@ -22,9 +20,15 @@ public class TP2_manip_CHAUVET_BAYLE {
         System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
         Tartiflette temporaire = assiette1;
         assiette1 = assiette2;
-        assiette2 = temporaire;
+        assiette2 = temporaire; //L'assiette 2 prend temporairement la valeur de l'assiette 1, ce sont toujours 2 objet différent 
         System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
         System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        /*
+        -L'assiette 666 ne peut pas marcher, on ne peut pas lui associer de valeur provenant d'une autre classe, ici Tartiflette
+        -L'assiette 667 ne peut pas marcher, on ne peut pas créer de valeur provenant d'une autre classe, ici Tartiflette
+        Il faudrait déjà associer les 2 classes pour que les 2 objets puissent interagir
+        -Une référence objet qui annonce référencer un type d’objet ne peut pas en référencer un autre qui n’a aucun rapport
+        */
         Moussaka [] tableau = new Moussaka[10];
         for (int i=0; i<10; i++) {
             tableau[i] = new Moussaka(100*i);
