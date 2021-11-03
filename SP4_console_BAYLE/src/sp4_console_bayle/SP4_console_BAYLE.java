c44326be98d5bcf3a9aecedbbae1656fe377506e
shirl <shirl@DESKTOP-FJJIUNF>
2021-11-03 22:28:34 +0100

    test

--- SP4_console_BAYLE/src/sp4_console_bayle/SP4_console_BAYLE.java
+++ SP4_console_BAYLE/src/sp4_console_bayle/SP4_console_BAYLE.java
@@ -13,7 +13,7 @@ import java.util.Random;
  */
 public class SP4_console_BAYLE {
 
-    /**
+    /** TEST A ENLEVER
      * @param args the command line arguments
      */
     public static void main(String[] args) {
